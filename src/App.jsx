32af37e531a8560f4e55350e524219444d3481a9
1tasnem <tasnemprodhan@gmail.com>
2025-04-25 12:36:54 +0600

    added last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import './index.css'; 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
+import Bookmarks from './components/Bookmarks/Bookmarks';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    
-      <h1 className='text-6xl bg-red-600'>Knowledge Cafe</h1>
-      
+  <Header></Header>
+  <div className="md:flex">
+  <Blogs></Blogs>
+  <Bookmarks></Bookmarks>
+  </div>
     </>
   )
 }
